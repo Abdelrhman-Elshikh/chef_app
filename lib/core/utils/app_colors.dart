8f47eac020ccbb59476f8acc2fb777dd935bbaa5
Abdelrhman-Elshikh <abdelrhmanelshikh@gmail.com>
2024-12-01 21:06:57 +0200

    add app colors & images

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color KPrimaryColor = Color(0xffFA9A0C);
+  static const Color KBlackColor = Color.fromRGBO(30, 30, 30, 1);
+  static const Color KWhiteColor = Color(0xffFFFFFF);
+  static const Color KGreyColor = Color(0xff9D9FA0);
+}
---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAvatar
+  /// assets/images/avatar.svg
+  static const String imagesAvatar = "assets/images/avatar.svg";
+
+  /// Assets for imagesChangeLanguageBg
+  /// assets/images/change_language_bg.svg
+  static const String imagesChangeLanguageBg = "assets/images/change_language_bg.svg";
+
+  /// Assets for imagesLock
+  /// assets/images/lock.svg
+  static const String imagesLock = "assets/images/lock.svg";
+
+  /// Assets for imagesLoginBg
+  /// assets/images/login_bg.svg
+  static const String imagesLoginBg = "assets/images/login_bg.svg";
+
+  /// Assets for imagesLogo
+  /// assets/images/logo.svg
+  static const String imagesLogo = "assets/images/logo.svg";
+}
+
