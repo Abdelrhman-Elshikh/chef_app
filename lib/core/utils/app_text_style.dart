47d6188052047992092acd5d104d3bb533342008
Abdelrhman-Elshikh <abdelrhmanelshikh@gmail.com>
2024-12-01 21:27:02 +0200

    app text style

---
+++ lib/core/utils/app_text_style.dart
@@ -0,0 +1,27 @@
+import 'package:chef_app/core/utils/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle _textStyle({
+  required Color color,
+  required FontWeight fontWeight,
+  required double fontSize,
+}) {
+  return GoogleFonts.lato(
+    color: color,
+    fontWeight: fontWeight,
+    fontSize: fontSize,
+  );
+}
+
+TextStyle boldStyle({
+  Color color = AppColors.KBlackColor,
+  double fontSize = 24,
+}) =>
+    _textStyle(color: color, fontWeight: FontWeight.bold, fontSize: fontSize);
+
+TextStyle regularStyle({
+  Color color = AppColors.KBlackColor,
+  double fontSize = 16,
+}) =>
+    _textStyle(color: color, fontWeight: FontWeight.normal, fontSize: fontSize);
